a70c3066ae6dd9213131d7e80b911107bf770f3d
Aaron Brown <abrown@amplify.com>
2015-03-23 10:51:05 -0400

    More parameterization with rainbows

--- update_stacks.sh
+++ update_stacks.sh
@@ -2,5 +2,20 @@
 
 export AWS_PROFILE=ab
 
-aws --profile $AWS_PROFILE cloudformation update-stack --stack-name vpc --template-body file://cfn-vpc.json --capabilities CAPABILITY_IAM
-rainbow -d cfn_resources:vpc -d yaml:coreos.yaml --update-stack-if-exists coreos coreos-alpha-pv.json --block
+
+STACKNAME='vpc'
+echo "Updating $STACKNAME Stack"
+rainbow -d yaml:${STACKNAME}.yaml \
+        --update-stack-if-exists \
+        --block \
+        $STACKNAME cfn-vpc.json
+
+echo "----------"
+
+STACKNAME='coreos'
+echo "Updating $STACKNAME Stack"
+rainbow -d cfn_resources:vpc \
+        -d yaml:${STACKNAME}.yaml \
+        --update-stack-if-exists \
+        --block \
+        $STACKNAME coreos-alpha-pv.json
